ce2e620284d1b504b0f81e93dfedbbd2f73af632
Nicky Hannaway <ext_nicky.hannaway@coats.com>
2020-08-27 18:18:07 +0100

    [NH] Made initial commit

---
+++ src/test/java/lme/coding/challenge/martian/robots/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lme.coding.challenge.martian.robots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
